3c3c395dded284162c2fb57e216bbb9b1bd998c8
Vivek Kumar <kumarvivek06071999@gmail.com>
2024-01-28 12:43:03 +0530

    App.jsx = Classbase from functional component

--- 07_List/src/App.jsx
+++ 07_List/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { Component } from 'react'
 import './App.css'
 import Card from './components/Card'
 import { faker, it } from '@faker-js/faker'
@@ -21,35 +21,36 @@ const Button = styled.button`
   transition: border-color 0.25s;
 `
 
-function App() {
-  const [showCard, setShowCard] = useState(true)
-  // Rendering list of Cards from array of objects
-  const [cards, setCards] = useState([
-    {
-      id: 'asdfasf',
-      name: 'Rahul Kumar',
-      avatar: 'https://loremflickr.com/640/480/people?lock=1345466532888576'
-    },
-    {
-      id: 'asdfdsfdf',
-      name: 'Vivek Kumar',
-      avatar: 'https://m.media-amazon.com/images/S/pv-target-images/ae4816cade1a5b7f29787d0b89610132c72c7747041481c6619b9cc3302c0101.jpg'
-    },
-    {
-      id: 'asdfsdfgdf',
-      name: 'Sunil Kumar',
-      avatar: 'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj'
-    },
-  ])
-
-  const deleteHandler = (cardIndex) => {
-    // here is reference type const cards_copy = cards if you want to change original use spread operator
-    const cards_copy = [...cards]
-    cards_copy.splice(cardIndex, 1)
-    console.log("cards_copy", cards_copy)
-    setCards(cards_copy)
-
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cards: [
+        {
+          id: 'asdfasf',
+          name: 'Rahul Kumar',
+          avatar: 'https://loremflickr.com/640/480/people?lock=1345466532888576'
+        },
+        {
+          id: 'asdfdsfdf',
+          name: 'Vivek Kumar',
+          avatar: 'https://m.media-amazon.com/images/S/pv-target-images/ae4816cade1a5b7f29787d0b89610132c72c7747041481c6619b9cc3302c0101.jpg'
+        },
+        {
+          id: 'asdfsdfgdf',
+          name: 'Sunil Kumar',
+          avatar: 'https://yt3.googleusercontent.com/-CFTJHU7fEWb7BYEb6Jh9gm1EpetvVGQqtof0Rbh-VQRIznYYKJxCaqv_9HeBcmJmIsp2vOO9JU=s900-c-k-c0x00ffffff-no-rj'
+        },
+      ],
+      showCard: true
+    }
   }
+
+
+  // Rendering list of Cards from array of objects
+
+
+
 
   // const buttonStyle = {
   //   backgroundColor: null
@@ -58,48 +59,74 @@ function App() {
   // if (cards.length < 3) { buttonStyle.backgroundColor = 'blue' }
   // if (cards.length < 2) { buttonStyle.backgroundColor = 'red' }
 
-  const classes = ["button"]
-  if (cards.length < 3) classes.push('blue')
-  if (cards.length < 2) classes.push('red')
+  // const classes = ["button"]
+  // if (cards.length < 3) classes.push('blue')
+  // if (cards.length < 2) classes.push('red')
 
-  const changeInputHandler = (e, id) => {
+  changeInputHandler = (e, id) => {
     // 1. Which card
-    const cardIndex = cards.findIndex((card) => card.id == id)
+    const cardIndex = this.state.cards.findIndex((card) => card.id == id)
 
     // 2. Make a copy of a card
-    const cards_copy = [...cards]
+    const cards_copy = [...this.state.cards]
 
     // 3. Change the name of a specific card
     cards_copy[cardIndex].name = e.target.value
     // 4. Set the cards with the latest version of card copy
-    setCards(cards_copy)
+    this.setState({ cards: cards_copy })
+    // setCards(cards_copy)
 
   }
 
   // Conditional Rendering here 
-  const cardMarkup = showCard && (
-    cards.map((item, index) => <Card
-      key={item.id}
-      name={item.name}
-      profile={item.avatar}
-      discription={faker.lorem.sentence(10)}
-      onDelete={() => deleteHandler(index)}
-      onChangeName={(e) => changeInputHandler(e, item.id)}
-      btnstyle={classes}
 
-    />)
-  )
+  render() {
+    const buttonStyle = {
+      backgroundColor: null
+    }
+
+    if (this.state.cards.length < 3) { buttonStyle.backgroundColor = 'blue' }
+    if (this.state.cards.length < 2) { buttonStyle.backgroundColor = 'red' }
+
+    const classes = ["button"]
+    if (this.state.cards.length < 3) classes.push('blue')
+    if (this.state.cards.length < 2) classes.push('red')
+
+    const deleteHandler = (cardIndex) => {
+      // here is reference type const cards_copy = cards if you want to change original use spread operator
+      const cards_copy = [...this.state.cards]
+      cards_copy.splice(cardIndex, 1)
+      this.setState({ cards: cards_copy })
 
 
-  return (
-    <ThemeProvider theme={theme}>
-      <>
-        <h2>Cards</h2>
-        <Button color='primary' length={cards.length}>Toggle</Button>
-        {cardMarkup}
-      </>
-    </ThemeProvider>
-  )
+    }
+
+    const cardMarkup = this.state.showCard && (
+      this.state.cards.map((item, index) => <Card
+        key={item.id}
+        name={item.name}
+        profile={item.avatar}
+        discription={faker.lorem.sentence(10)}
+        onDelete={() => deleteHandler(index)}
+        onChangeName={(e) => changeInputHandler(e, item.id)}
+        btnstyle={classes}
+
+      />)
+    )
+
+
+    return (
+      <ThemeProvider theme={theme}>
+        <>
+          <h2>Cards</h2>
+          <Button color='primary' length={this.state.cards.length}>Toggle</Button>
+          {cardMarkup}
+        </>
+      </ThemeProvider>
+    )
+  }
+
+
 }
 
 export default App
