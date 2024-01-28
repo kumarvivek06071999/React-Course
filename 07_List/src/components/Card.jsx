77e5c716c1a998e4f6423028f6e3c8785f3b963d
Vivek Kumar <kumarvivek06071999@gmail.com>
2024-01-28 13:24:30 +0530

    Card Change from functional component to class component

--- 07_List/src/components/Card.jsx
+++ 07_List/src/components/Card.jsx
@@ -1,34 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 
-const Card = (props) => {
-    return (
-        <div className="relative h-[400px] w-[300px] rounded-md mt-4">
+class Card extends Component {
 
-            <img
-                src={props.profile}
-                alt="AirMax Pro"
-                className="z-0 h-full w-full rounded-md object-cover"
-            />
-            <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
-            <div onClick={props.onChangehandler} className="absolute bottom-4 left-4 text-left">
-                <h1 className="text-lg font-semibold text-white">{props.name}</h1>
+    render() {
+        return (
+            <div className="relative h-[400px] w-[300px] rounded-md mt-4">
 
-                {/*  Two-way data binding allows bidirectional data flow, meaning that changes in the UI automatically update the component's state, and changes in the state automatically update the UI */}
-                <input type="text" value={props.name} className='  px-3 py-2' onChange={props.onChangeName} />
+                <img
+                    src={this.props.profile}
+                    alt="AirMax Pro"
+                    className="z-0 h-full w-full rounded-md object-cover"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
+                <div onClick={this.props.onChangehandler} className="absolute bottom-4 left-4 text-left">
+                    <h1 className="text-lg font-semibold text-white">{this.props.name}</h1>
 
-                <p className="mt-2 text-sm text-gray-300">
-                    {props.discription}
-                </p>
-                <button onClick={props.onDelete}
-                    // style={props.btnstyle}
-                    className={props.btnstyle.join(" ")}
-                >
-                    Delete Card
-                </button>
+                    {/*  Two-way data binding allows bidirectional data flow, meaning that changes in the UI automatically update the component's state, and changes in the state automatically update the UI */}
+                    <input type="text" value={this.props.name} className='  px-3 py-2' onChange={this.props.onChangeName} />
+
+                    <p className="mt-2 text-sm text-gray-300">
+                        {this.props.discription}
+                    </p>
+                    <button onClick={this.props.onDelete}
+                        // style={this.props.btnstyle}
+                        className={this.props.btnstyle.join(" ")}
+                    >
+                        Delete Card
+                    </button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+
+
 }
 
 export default Card
